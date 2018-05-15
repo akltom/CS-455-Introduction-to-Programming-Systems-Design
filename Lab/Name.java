ddb9935509a2528587c48c9ac625109982cbe500
ImAKing <kla283@nyu.edu>
2018-05-15 00:30:17 -0700

    Delete Name.java

--- Lab/Name.java
+++
@@ -1,8 +0,0 @@
-public class Name {
-    public static void main(String[] args) {
-
-	System.out.println("My name is Joe Blow.");
-        System.out.println("My major is underwater basket-weaving.");
-	System.out.println("I am a PhD student.");
-	    }
-}
