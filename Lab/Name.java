2f2d9b564c804477349ff30b2b6bbcd2694032b0
ImAKing <kla283@nyu.edu>
2018-05-15 00:12:05 -0700

    Add files via upload

---
+++ Lab/Name.java
@@ -0,0 +1,8 @@
+public class Name {
+    public static void main(String[] args) {
+
+	System.out.println("My name is Joe Blow.");
+        System.out.println("My major is underwater basket-weaving.");
+	System.out.println("I am a PhD student.");
+	    }
+}
